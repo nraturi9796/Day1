bfaefbcd6d5539206e3d0964036db0c2860b818d
nraturi9796 <152674283+nraturi9796@users.noreply.github.com>
2024-06-26 05:42:30 +0000

    change

---
+++ index.js
@@ -0,0 +1,40 @@
+//console.log("hello");
+
+// let a=40;
+// let b=40;
+// console.log("a =" ,a," & b=",b);
+// console.log("a+b =" ,a+b);
+// console.log("a-b =",a-b);
+// console.log("a*b =" ,a*b);
+// console.log("a/b =" ,a/b);
+
+// let x=3;
+// let y=6;
+// console.log("a =" ,a,"& b =" ,b);
+// console.log("a%b =" ,a%b);
+
+// let f=10;
+// let g=30;
+// console.log("f =",f,"& g = " ,g);
+// // console.log(" f+g =",f+g);
+
+// let x=40;
+// let y=4;
+// console.log("x=",x,"&y=",y);
+// console.log("x%y=",x%y);
+
+// let a=2;
+// let b=3;
+// console.log("a=",a,"& b=",b);
+// console.log("a**b=",a**b);
+
+//Unary operator
+//for increment
+
+// let a=5;
+// console.log("a+1=",a+1);
+
+// //for decrement
+
+// let x=30;
+// console.log("x-1=",x-1);
