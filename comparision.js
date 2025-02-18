4bb63d3bb6d9a0c42a1a6c7abaced3f14ed36221
nraturi9796 <152674283+nraturi9796@users.noreply.github.com>
2025-02-18 13:38:34 +0000

    save

---
+++ comparision.js
@@ -0,0 +1,12 @@
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(undefined <=0);
+console.log(0>=undefined);
+
+// It is not good comparision.The best comparision is compare same date type
+
+console.log(10>5);
+console.log("a">"b");
+console.log("b"<"a");
