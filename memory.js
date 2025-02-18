a74af1d02a0f519f7e93d9a146a3c79a70221f7a
nraturi9796 <152674283+nraturi9796@users.noreply.github.com>
2025-02-18 15:39:37 +0000

    add

---
+++ memory.js
@@ -0,0 +1,25 @@
+//-------------------------------------Stack memory------------------
+
+// all primitive datatype store in stack.Stack is work on the mechanism of LIFO(Last in Firsst Out)
+let name="Nitesh"
+let LastName=name
+LastName="Raturi"
+console.log(name);
+console.log(name);
+
+//-----------------------------------------Heap -----------------------------
+
+//All non primitivedatatype store in heap area.like object,function,array
+
+let userone={
+    email:"nraturi246@gmail.com";
+    password:"nraturi246@"
+}
+console.log(userone);
+
+// let usertwo=userone
+// usertwo.email="nraturi1234@gmail.com";
+// console.log(userone);
+// console.log(usertwo);
+
+
