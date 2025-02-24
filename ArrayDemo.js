3be9f5e5e5a500f90fb86c3dde36e107e090ba4d
nraturi9796 <152674283+nraturi9796@users.noreply.github.com>
2025-02-24 17:18:09 +0000

    add

---
+++ ArrayDemo.js
@@ -0,0 +1,8 @@
+// const arrs=["Nitesh","Anjali","Pali","Gahar",]
+// const arrs2=["Ram","Shita","Laxhman","hanuman","Bharat"]
+// //console.log(arrs[3][2]);
+
+// console.log(arrs.concat(arrs2));
+
+
+
