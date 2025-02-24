64a374c7034ed14efae70521d31618cce11c6318
nraturi9796 <152674283+nraturi9796@users.noreply.github.com>
2025-02-24 16:15:31 +0000

    add

---
+++ Arrays.js
@@ -0,0 +1,41 @@
+// const arr=[11,13,12,16,19]
+// console.log(arr);
+// console.log(arr[3]);
+
+const arrs=new Array(12,14,16,18,20);
+// console.log(arrs);
+// arrs.push(21);
+// console.log(arrs);
+
+// arrs.pop();
+// console.log(arrs);
+
+
+// console.log(arrs.indexOf(12));
+// console.log(arrs.slice(2,6));
+// arrs.splice(2,0,"213")
+// console.log(arrs);
+
+const remove=arrs.splice(2,1)
+console.log(remove);
+console.log(arrs);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
