a8414fe3f188fd341559151f7ff3f3502d946472
nraturi9796 <152674283+nraturi9796@users.noreply.github.com>
2025-02-27 06:01:05 +0000

    add

--- Arrays.js
+++ Arrays.js
@@ -2,7 +2,7 @@
 // console.log(arr);
 // console.log(arr[3]);
 
-const arrs=new Array(12,14,16,18,20);
+//const arrs=new Array(12,14,16,18,20);
 // console.log(arrs);
 // arrs.push(21);
 // console.log(arrs);
@@ -16,9 +16,20 @@ const arrs=new Array(12,14,16,18,20);
 // arrs.splice(2,0,"213")
 // console.log(arrs);
 
-const remove=arrs.splice(2,1)
-console.log(remove);
-console.log(arrs);
+// const remove=arrs.splice(2,1)
+// console.log(remove);
+// console.log(arrs);
+
+// const another =[12,23,25,[27,29,13],43,[1,9]];
+// const fix=another.flat(Infinity)
+// console.log(fix);
+
+//console.log(Array.isArray("Nitesh"));
+ //console.log(Array.from("Nitesh"));
+//console.log(Array.from({name:"Nitesh"}));
+
+
+
 
 
 
